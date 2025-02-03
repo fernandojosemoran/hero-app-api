6454e42ff7c688063a6da93cfab5240e1f78aa8b
fernando <fernandomoran323@gmail.com>
2025-02-03 00:33:04 -0800

    refactor: new heroApp angular project build

--- statics/out/chunk-25A5QXYH.js
+++
@@ -1,1 +0,0 @@
-import{b as a}from"./chunk-NJZI3YBR.js";import"./chunk-XEXMLEVJ.js";import"./chunk-LAEVFYIO.js";export{a as AuthModule};
--- statics/out/chunk-J72XCHJC.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-DNNOOLEY.js";import"./chunk-XEXMLEVJ.js";import"./chunk-LAEVFYIO.js";export{a as HeroesModule};
