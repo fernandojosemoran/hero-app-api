69f11ae4b20ab7420f485c4659cf2312e8f7ae7a
fernando <fernandomoran323@gmail.com>
2025-02-02 21:06:22 -0800

    refactor: add out folder

---
+++ statics/out/chunk-25A5QXYH.js
@@ -0,0 +1,1 @@
+import{b as a}from"./chunk-NJZI3YBR.js";import"./chunk-XEXMLEVJ.js";import"./chunk-LAEVFYIO.js";export{a as AuthModule};
---
+++ statics/out/chunk-J72XCHJC.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-DNNOOLEY.js";import"./chunk-XEXMLEVJ.js";import"./chunk-LAEVFYIO.js";export{a as HeroesModule};
