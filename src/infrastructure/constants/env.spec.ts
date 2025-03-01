912278cab7f572f2a32755794e4ae876cb7363c9
fernando <fernandomoran323@gmail.com>
2025-03-01 01:26:14 -0800

    test: added test for envs.ts

--- src/infrastructure/constants/env.spec.ts
+++ src/infrastructure/constants/env.spec.ts
@@ -1,5 +1,37 @@
-describe('Indicate the directory of the file to test', () => {
-    test('Indicate to make the test', () => {
-        expect(true).toBe(true);
+import Env from "./env";
+
+describe('./src/infrastructure/constants/env.ts', () => {
+    test(`Should have ${Object.keys(Env).length} properties`, () => {
+        expect(Object.keys(Env)).toHaveLength(13);
+    });
+
+    test('Should have system envs like PORT, DEBUG, HOST_URL, MODE_TEST, OWNER_TO_SEND_EMAIL_TEST', () => {
+        expect(Env).toHaveProperty("PORT");
+        expect(Env).toHaveProperty("DEBUG");
+        expect(Env).toHaveProperty("MODE_TEST");
+        expect(Env).toHaveProperty("OWNER_TO_SEND_EMAIL_TEST");
+    });
+
+    test('Should have lists allowed envs like WHITE_LIST_ALLOWED_HOSTS_DEV, WHITE_LIST_ALLOWED_HOSTS_PROD', () => {
+        expect(Env).toHaveProperty("WHITE_LIST_ALLOWED_HOSTS_DEV");
+        expect(Env).toHaveProperty("WHITE_LIST_ALLOWED_HOSTS_PROD");
+    });
+
+    test('Should have tokens envs like JWT_SECRET_KEY', () => {
+        expect(Env).toHaveProperty("JWT_SECRET_KEY");
+    });
+
+    test('Should have cookies envs like COOKIE_SECRET_KEY', () => {
+        expect(Env).toHaveProperty("COOKIE_SECRET_KEY");
+    });
+    
+    test('Should have email envs like MAILER_SERVICE, MAILER_EMAIL, MAILER_SECRET_KEY', () => {
+        expect(Env).toHaveProperty("MAILER_SERVICE");
+        expect(Env).toHaveProperty("MAILER_EMAIL");
+        expect(Env).toHaveProperty("MAILER_SECRET_KEY");
+    });
+
+    test('Should have rate limit envs like RATE_LIMIT', () => {
+        expect(Env).toHaveProperty("RATE_LIMIT");
     });
 });
--- src/infrastructure/constants/env.ts
+++ src/infrastructure/constants/env.ts
@@ -4,18 +4,29 @@ import dotenv from "dotenv";
 dotenv.config();
 
 class Env {
+    // system
     public static readonly PORT: number = env.get("PORT").required().default(5000).asPortNumber();
     public static readonly DEBUG: boolean = env.get("DEBUG").default("true").asBool();
+    public static readonly HOST_URL: string = !this.DEBUG ? env.get("HOST_URL").required().asUrlString() : `http://127.0.0.1:${this.PORT}`;
+    public static readonly MODE_TEST: boolean = env.get("MODE_TEST").default("false").required().asBool();
+    public static readonly OWNER_TO_SEND_EMAIL_TEST: string | undefined = env.get("OWNER_TO_SEND_EMAIL_TEST").asEmailString();
+    
+    // lists allowed
     public static readonly WHITE_LIST_ALLOWED_HOSTS_DEV: string[] = env.get("WHITE_LIST_ALLOWED_HOSTS_DEV").required().asArray();
     public static readonly WHITE_LIST_ALLOWED_HOSTS_PROD: string[] = env.get("WHITE_LIST_ALLOWED_HOSTS_PROD").required().asArray();
+    
+    // tokens
     public static readonly JWT_SECRET_KEY: string = env.get("JWT_SECRET_KEY").required().asString();
+    
+    // cookies
     public static readonly COOKIE_SECRET_KEY: string = env.get("COOKIE_SECRET_KEY").required().asString();
+    
+    // email
     public static readonly MAILER_SERVICE: string = env.get("MAILER_SERVICE").required().asString();
     public static readonly MAILER_EMAIL: string = env.get("MAILER_EMAIL").required().asString();
     public static readonly MAILER_SECRET_KEY: string = env.get("MAILER_SECRET_KEY").required().asString();
-    public static readonly HOST_URL: string = !this.DEBUG ? env.get("HOST_URL").required().asUrlString() : `http://127.0.0.1:${this.PORT}`;
-    public static readonly OWNER_TO_SEND_EMAIL_TEST: string | undefined = env.get("OWNER_TO_SEND_EMAIL_TEST").asEmailString();
-    public static readonly MODE_TEST: boolean = env.get("MODE_TEST").default("false").required().asBool();
+    
+    // rate limit
     public static readonly RATE_LIMIT: number = env.get("RATE_LIMIT").required().asInt();
 }
 
