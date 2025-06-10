34fc882fa7ed9477d393b0c831be290d973d4f07
fernando <fernandomoran323@gmail.com>
2025-06-10 11:46:52 -0700

    refactor: replaced the static files of out folder

---
+++ statics/out/chunk-BPQ4H5OM.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-EFHGLZO2.js";import"./chunk-WJEOTJLG.js";import"./chunk-ZIFPUKJ6.js";export{a as AuthModule};
--- statics/out/chunk-J6ECGXTF.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-2A4ZCXSK.js";import"./chunk-DTLXTEED.js";import"./chunk-ZIFPUKJ6.js";export{a as HeroesModule};
